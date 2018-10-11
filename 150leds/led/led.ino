901e8d0513bad17e56b87ffdb9ef9daaa6b6cbd0
Lukas van Driel <lukas@q42.nl>
2018-10-11 07:36:30 +0200

    minder leds, nachtlamp modus

--- 150leds/led/led.ino
+++ 150leds/led/led.ino
@@ -3,10 +3,26 @@
   #include <avr/power.h>
 #endif
 
-#define NUM_LIGHTS 150
-#define PIN 6
+#define NUM_LIGHTS 118
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LIGHTS, PIN, NEO_GRB + NEO_KHZ800);
+#define LIGHTS_PIN 6
+#define TEST_BUTTON 12
+
+// uit-arduino, utp-pin, knop-draad-kleur, knop-kleur
+
+// blauw, 5, geel, blauw
+#define BUTTON_BLUE 8
+// bruin, 4, groen, roze
+#define BUTTON_PINK 9
+// grijs, 3, blauw, geel
+#define BUTTON_YELLOW 10
+// wit, 2, bruin, groen
+#define BUTTON_GREEN 11
+// zwart, 1, grijs
+// GRND
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LIGHTS, LIGHTS_PIN, NEO_GRB + NEO_KHZ800);
+int greenCounter = 0;
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
@@ -14,12 +30,51 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LIGHTS, PIN, NEO_GRB + NEO_KHZ80
 // on a live circuit...if you must, connect GND first.
 
 void setup() {
+  pinMode(BUTTON_BLUE, INPUT_PULLUP);
+  pinMode(BUTTON_PINK, INPUT_PULLUP);
+  pinMode(BUTTON_YELLOW, INPUT_PULLUP);
+  pinMode(BUTTON_GREEN, INPUT_PULLUP);
+  pinMode(TEST_BUTTON, INPUT_PULLUP);
+  
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
+
+  colorWipe(strip.Color(255, 0, 0), 3); // Red
+  colorWipe(strip.Color(0, 0, 0), 3); // Red
 }
 
 void loop() {
-//  colorWipe(strip.Color(255, 0, 0), 20); // Red
+  if (digitalRead(TEST_BUTTON) == LOW) {
+    colorWipe(strip.Color(255, 0, 0), 3); // Red
+    colorWipe(strip.Color(0, 0, 0), 3); // Red
+  }
+  
+  if (digitalRead(BUTTON_BLUE) == LOW) {
+    rainbowCycle(20);
+  }
+
+  if (digitalRead(BUTTON_PINK) == LOW) {
+    police2(15, 700);
+  }
+
+  if (digitalRead(BUTTON_YELLOW) == LOW) {
+    police1slow(10);
+  }
+
+  if (digitalRead(BUTTON_GREEN) == LOW) {
+    if (greenCounter == 0) {
+      nightlight();
+    } else {
+      allOff();
+    }
+    
+    greenCounter++;
+    if (greenCounter == 2) greenCounter = 0;
+  }
+
+  strip.show();
+
+//  knightRider(5, 20);
 //  colorWipe(strip.Color(0, 255, 0), 20); // Green
 //  colorWipe(strip.Color(0, 0, 255), 20); // Blue
 
@@ -30,15 +85,6 @@ void loop() {
 //  rainbow(20);
 //  rainbowCycle(20);
 //  theaterChaseRainbow(50);
-
-
-    police1(10, 50);
-
-    police2(15, 200);
-
-    knightRider(5, 20);
-
-    delay(5000);
 }
 
 void police2(int count, int duration) {
@@ -70,31 +116,49 @@ void police2(int count, int duration) {
   allOff();
 }
 
-void police1(int count, int duration) {
+void police1slow(int count) {
   uint32_t blue = strip.Color(0, 0, 255); 
   uint32_t red = strip.Color(255,0,0);
+  int lightDuration = 300;
+  int delayDuration = 75;
 
   for (int i = 0; i < count; i++) {
-    police1Blink(0, red, duration*2);
-    delay(duration);
-    police1Blink(0, red, duration*2);
-    delay(duration);
-    police1Blink(75, blue, duration*2);
-    delay(duration);
-    police1Blink(75, blue, duration*2);
-    delay(duration);
+    police1Blink(0, red, lightDuration);
+    delay(delayDuration);
+    police1Blink((NUM_LIGHTS / 2), blue, lightDuration);
+    delay(delayDuration);
+  }
+
+  allOff();
+}
+
+void police1fast(int count) {
+  uint32_t blue = strip.Color(0, 0, 255); 
+  uint32_t red = strip.Color(255,0,0);
+  int lightDuration = 75;
+  int delayDuration = 75;
+
+  for (int i = 0; i < count; i++) {
+    police1Blink(0, red, lightDuration);
+    delay(delayDuration);
+    police1Blink(0, red, lightDuration);
+    delay(delayDuration*2);
+    police1Blink((NUM_LIGHTS / 2), blue, lightDuration);
+    delay(delayDuration);
+    police1Blink((NUM_LIGHTS / 2), blue, lightDuration);
+    delay(delayDuration*2);
   }
 
   allOff();
 }
 
 void police1Blink(int startLed, uint32_t color, int duration) {
-  for (int i = startLed; i < startLed + 75; i++) {
+  for (int i = startLed; i < startLed + (NUM_LIGHTS / 2); i++) {
     strip.setPixelColor(i, color);
   }
   strip.show();
   delay(duration);
-  for (int i = startLed; i < startLed + 75; i++) {
+  for (int i = startLed; i < startLed + (NUM_LIGHTS / 2); i++) {
     strip.setPixelColor(i, strip.Color(0, 0, 0));
   }
   strip.show();
@@ -129,9 +193,20 @@ void knightRider(int loops, int timer) {
     allOff();
 }
 
+void nightlight() {
+  for(int i = 0; i < 10; i++){
+    allColor(strip.Color(i, 0, 0));
+    delay(20);
+  }
+}
+
 void allOff() {
-  for( int i = 0; i<NUM_LIGHTS; i++){
-    strip.setPixelColor(i, strip.Color(0, 0, 0));
+  allColor(strip.Color(0, 0, 0));
+}
+
+void allColor(uint32_t color) {
+  for(int i = 0; i<NUM_LIGHTS; i++){
+    strip.setPixelColor(i, color);
   }   
   strip.show();
 }
@@ -164,19 +239,21 @@ void rainbow(uint8_t wait) {
     strip.show();
     delay(wait);
   }
+  allOff();
 }
 
 // Slightly different, this makes the rainbow equally distributed throughout
 void rainbowCycle(uint8_t wait) {
   uint16_t i, j;
 
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+  for(j=0; j<256*4; j++) { // 5 cycles of all colors on wheel
     for(i=0; i< strip.numPixels(); i++) {
       strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
     }
     strip.show();
     delay(wait);
   }
+  allOff();
 }
 
 //Theatre-style crawling lights.
@@ -195,6 +272,7 @@ void theaterChase(uint32_t c, uint8_t wait) {
       }
     }
   }
+  allOff();
 }
 
 //Theatre-style crawling lights with rainbow effect
@@ -213,6 +291,7 @@ void theaterChaseRainbow(uint8_t wait) {
       }
     }
   }
+  allOff();
 }
 
 // Input a value 0 to 255 to get a color value.
